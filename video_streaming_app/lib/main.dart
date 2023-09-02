be089b8ec5b0826b3e5d1d18b65865e7ee75734e
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-09-02 17:58:36 +0530

    added the rootscreen

--- video_streaming_app/lib/main.dart
+++ video_streaming_app/lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+import 'screen/root_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RootScreen(),
+    ),
+  );
+}
